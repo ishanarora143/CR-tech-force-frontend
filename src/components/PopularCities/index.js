7b57bc214fc3868332c017e7f9fa8f8841571898
Vaibhav Arora <varora1406@gmail.com>
2021-05-02 00:42:36 +0530

    fix: 🐛 import not done for logEvent

--- src/components/PopularCities/index.js
+++ src/components/PopularCities/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { withRouter } from 'react-router';
+import { logEvent } from '../../utils/gtag';
 import Button from '../Button';
 import ROUTES from './../../constants/routes';
 import { Context as SearchContext } from './../../context/SearchContext';
